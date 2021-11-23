3d603f3367a5d713555afd19ce5fd68abe7caefb
Liath <71844824+LiathDevelopment@users.noreply.github.com>
2021-11-23 18:08:57 +0000

    Rework user level data handling

--- datahandler.py
+++ datahandler.py
@@ -1,44 +1,45 @@
 from gzip import open as gzipopen
 from json import load, dump
+from os import path, makedirs
 
 def updateusermap(username, map_id, map_data):
-    data = load(open("./data.json", "r"))
-    data["users"][username]["maps"][str(map_id)] = map_data
-    dump(data, open("./data.json", "w"), indent=4)
+    data = load(open(f"./levels/{username}/data.json", "r"))
+    data["levels"][str(map_id)] = map_data
+    dump(data, open(f"./levels/{username}/data.json", "w"), indent=4)
 
 def saveusermap(username, map_id, map_data):
     print(f"Writing {username}_{map_id}.dat")
-    with gzipopen(f"./maps/{username}_{map_id}.dat", "w") as f:
+    with gzipopen(f"./levels/{username}/level_{map_id}.dat", "w") as f:
         f.write(map_data)
     f.close()
 
 def getusermaps(username):
-    data = load(open("./data.json", "r"))
     map_string = ""
-    found_data = False
-    for x in data["users"]:
-        if x == username:
-            found_data = True
-            for y in range(5):
-                try:
-                    map_string += data["users"][username]["maps"][str(y)]["name"]
-                except:
-                    map_string += "-"
-                if y != 4:
-                    map_string += ";"
-    
-    if found_data == False:
+    try:
+        data = load(open(f"./levels/{username}/data.json", "r"))
+    except:
         adduserentry(username)
-        return "-;-;-;-;-"
+        data = load(open(f"./levels/{username}/data.json", "r"))
+
+    for y in range(5):
+        try:
+            map_string += data["levels"][str(y)]["name"]
+        except:
+            map_string += "-"
+        if y != 4:
+            map_string += ";"
+
     return(map_string)
 
 def getusermapdata(username, map_id):
-    with gzipopen(f"./maps/{username}_{map_id}.dat", "rb") as f:
+    with gzipopen(f"./levels/{username}/level_{map_id}.dat", "rb") as f:
         data = f.read()
     f.close()
     return data
 
 def adduserentry(username):
-    data = load(open("./data.json", "r"))
-    data["users"].update({f"{username}": {"maps": {}}})
-    dump(data, open("./data.json", "w"), indent=4)
+    if not path.exists(f"./levels/{username}/"):
+        makedirs(f"./levels/{username}/")
+        data = open(f"./levels/{username}/data.json", 'x')
+        data.write("{\"levels\": {}}")
+        data.close()
